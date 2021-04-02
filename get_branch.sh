03e31e046b09dc814dd87f1a945c0f8e1df3d548
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2021-04-01 20:31:16 -0400

    using return instead of exit fix shells sometime exiting

--- get_branch.sh
+++ get_branch.sh
@@ -6,11 +6,11 @@ function get_branch {
     if [[ -d .git ]]; then
       branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
       echo ">$branch"
-      exit 0
+      return 0
     fi
     cd ..
     path=$(pwd)
   done
-  exit 1
+  return 1
 }
 
