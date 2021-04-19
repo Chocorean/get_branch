ba5868c80c2f16901450b26bf1086b41b5713407
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2021-04-18 22:57:08 -0400

    fix #1

--- get_branch.sh
+++ get_branch.sh
@@ -5,12 +5,16 @@ function get_branch {
   while [[ "$path" != "/" ]]; do
     if [[ -d .git ]]; then
       branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+	  if [[ "$branch" == "HEAD" ]]
+	  then
+        branch="DH@$(git log | head -n1 | cut -d\  -f2 | cut -c 1-7)"
+	  fi
       echo ">$branch"
-      return 0
+      exit 0
     fi
     cd ..
     path=$(pwd)
   done
-  return 1
+  exit 1
 }
 
