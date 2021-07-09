94d6c2e53eae637dabf65e2b852df8a208c1529b
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2021-07-09 15:57:21 -0400

    Replace rev-parse by branch; fix #2

--- get_branch.sh
+++ get_branch.sh
@@ -4,8 +4,8 @@ function get_branch {
   path=$(pwd)
   while [[ "$path" != "/" ]]; do
     if [[ -d .git ]]; then
-      branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-	  if [[ "$branch" == "HEAD" ]]
+      branch=$(git branch 2> /dev/null | awk '{print $2}')
+	  if [[ "$branch" =~ .*detached.* ]]
 	  then
         branch="DH@$(git log | head -n1 | cut -d\  -f2 | cut -c 1-7)"
 	  fi
