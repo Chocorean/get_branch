c8732a7cd8492e1f8abc320ee30c2e08cb92077c
Chocorean <chocorean@pm.me>
2023-12-04 14:23:07 -0500

    add rebase case, better cmd outputs handling

--- get_branch.sh
+++ get_branch.sh
@@ -4,17 +4,22 @@ function get_branch {
   path=$(pwd)
   while [[ "$path" != "/" ]]; do
     if [[ -d .git ]]; then
-      branch=$(git branch 2> /dev/null | awk '{print $2}')
-	  if [[ "$branch" =~ .*detached.* ]]
-	  then
-        branch="DH@$(git log | head -n1 | cut -d\  -f2 | cut -c 1-7)"
-	  fi
-      echo ">$branch"
-      exit 0
+      # branch=$(git branch 2> /dev/null | awk '{print $2}')
+      branch="$(git branch 2> /dev/null)"
+      if [[ "$branch" =~ .*detached.* ]]  # detached HEAD
+      then
+        branch=" $(git log | head -n1 | cut -d\  -f2 | cut -c 1-7)"
+      elif [[ "$branch" =~ .*rebasing.* ]]  # during rebase
+      then
+        branch="$(echo "$branch" | head -n1 | sed -e 's/.*rebasing \(.*\))$/[rebase]\1/')"
+      else  # normal case
+        branch="$(git branch | awk '{print $2}')"
+      fi
+      echo " $branch"
+      return 0
     fi
     cd ..
     path=$(pwd)
   done
-  exit 1
+  return 1
 }
-
